800bbb678a640310546625433ad014f9f37eb451
andrew-May0047 <144417120+andrew-May0047@users.noreply.github.com>
2023-09-09 12:48:01 -0500

    Update debugging_assignment.cpp

--- debugging_assignment.cpp
+++ debugging_assignment.cpp
@@ -14,24 +14,18 @@ int main ()
     
     cout << "In Main " << endl;
     
-    for (int index = 0; index < SIZE ; index++)
+    for (int index = 0; index < SIZE; index++)
     {          
        cout << Names[index] << endl;
     }
 
     Method1();
-    cout << "In Main " << endl<<endl;
+    cout << "In Main" << endl << endl;
+    Method2();
+    cout << "In Main" << endl << endl;
     Method3();
-    cout << "In Main " << endl<<endl;
-
-    int temp = 4; 
-
-    for (int i = 1; i <= 10; i++)
-    { 
-       if (i > 6) 
-          temp = 5; 
-    }
-
+    cout << "In Main" << endl << endl;
+    
     std::cout <<"Press a key to exit" << endl;
     std::cin >> Pause; 
 
@@ -41,17 +35,15 @@ int main ()
 
 void Method1()
 { 
-    cout << ("Break Point in Method1") << endl;
-    Method2();
+    cout << ("Break Point in Method1") << endl; // BreakPoint
 }
 
 void Method2()
 {
-    cout <<("Break Point in Method2") << endl;  // BreakPoint
-    cout <<("Break Point in Method2") << endl;  // BreakPoint
+    cout << ("Break Point in Method2") << endl;  // BreakPoint
 }
 
 void Method3()
 {
-    cout <<("Break Point in Method3")<< endl;  // Breakpoint
+    cout << ("Break Point in Method3") << endl;  // Breakpoint
 }
